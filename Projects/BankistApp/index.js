97f0763572f6e025c9a851b9066491c85bd7286e
Abhishek kumar <aasalex21c@gmail.com>
2023-12-28 17:26:32 +0530

    added responsiveness !

--- Projects/BankistApp/index.js
+++ Projects/BankistApp/index.js
@@ -1,1 +1,9 @@
 'use strict';
+// Data
+const account1 = {
+  owner: 'Abhishek kumar',
+  transactions: [6200, -3500, 700, -500, 2100, -1800, 500, -300, 1100, -800],
+  interestRate: 1.2,
+  pin: 1111,
+};
+console.log(account1);
